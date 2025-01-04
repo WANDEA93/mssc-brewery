cfd653e9d42a804d8159bc86ab5820400d702130
Asitha De Alwis <w.dealwis@ucdconnect.ie>
2025-01-04 20:33:12 +0530

    Added support for Beer POST and Customer

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerDTO getById(UUID id) {
+        return CustomerDTO.builder()
+                .id(UUID.randomUUID())
+                .name("Jon Doe")
+                .build();
+    }
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import guru.springframework.msscbrewery.services.CustomerService;
+import guru.springframework.msscbrewery.web.model.CustomerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@RestController
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable("customerId") UUID customerId) {
+        var customer = this.customerService.getById(customerId);
+        return ResponseEntity.ok().body(customer);
+    }
+}
